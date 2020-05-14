c545a20d9cf70ed9b61005d2c325fe0e6a87a1e3
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-05-14 11:09:40 -0300

    feat: Included GSON library to convert Json object.

--- src/main/java/br/com/mtmogi/mtmogi/controller/JsonController.java
+++ src/main/java/br/com/mtmogi/mtmogi/controller/JsonController.java
@@ -1,9 +1,13 @@
 package br.com.mtmogi.mtmogi.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
 
 
 @RestController
@@ -13,7 +17,15 @@ public class JsonController {
 	@RequestMapping(value = "/compare",method = RequestMethod.POST)
 	public String upload(@RequestBody String payload) {
 		
-		System.out.println(payload);
+		Gson gson = new Gson();
+		
+		@SuppressWarnings("unchecked")
+		ArrayList<Long> idServers = gson.fromJson(payload, ArrayList.class);
+		
+		for (Object object : idServers) {
+			System.out.println(object);
+		}
+		
 		return "OK";
 	}
 	
