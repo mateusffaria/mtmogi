fdf957e7e94dc67d2d56a454097775afebccae71
rcdev99 <ricardo06.rodrigues@gmail.com>
2020-05-14 11:47:55 -0300

    feat: Created method to get servers using a list of ids.

--- src/main/java/br/com/mtmogi/mtmogi/controller/JsonController.java
+++ src/main/java/br/com/mtmogi/mtmogi/controller/JsonController.java
@@ -1,29 +1,51 @@
 package br.com.mtmogi.mtmogi.controller;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import br.com.mtmogi.mtmogi.model.Servidor;
+import br.com.mtmogi.mtmogi.service.ServiceImpl.MtmogiServiceImpl;
+
+
 
 
 @RestController
 @RequestMapping("/service")
 public class JsonController {
 
+	@Autowired
+	MtmogiServiceImpl mService;
+	
 	@RequestMapping(value = "/compare",method = RequestMethod.POST)
 	public String upload(@RequestBody String payload) {
 		
+		//Gson object instance
 		Gson gson = new Gson();
 		
-		@SuppressWarnings("unchecked")
-		ArrayList<Long> idServers = gson.fromJson(payload, ArrayList.class);
+		//Defining an object type for conversion
+		Type collectionType = new TypeToken<List<Long>>() {}.getType();
 		
-		for (Object object : idServers) {
-			System.out.println(object);
+		//Populating long array with defined object type
+		ArrayList<Long> idServers = gson.fromJson(payload, collectionType);
+		
+		//Server list instance
+		List<Servidor> servers = new ArrayList<Servidor>();
+		
+		//Populating Server array
+		servers = mService.findServers(idServers);
+		
+		for (Servidor servidor : servers) {
+			System.out.println(servidor.getNome() + " " + servidor.getFuncao());
 		}
 		
 		return "OK";
